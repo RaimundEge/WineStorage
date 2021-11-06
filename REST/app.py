d5635d581f058b319bd4c32d3c426bee05f41aaa
RaimundEge <ege@niu.edu>
2021-11-05 17:38:53 -0700

    filter query by range

--- REST/app.py
+++ REST/app.py
@@ -1,17 +1,22 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from pymongo import MongoClient
 import json
 from bson import json_util
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
+@cross_origin()
 def index():
+    print(request.args.get('range'))
     client = MongoClient('mongodb://localhost:27017/')
     db = client.local 
     coll = db.temps
-    #list = [doc for doc in coll.find()]
-    # print(list)
+    
     list = [{"when": doc['time'], "temp": doc['value']} for doc in coll.find()]
     print(str(len(list)) + ' temp records found')
 
