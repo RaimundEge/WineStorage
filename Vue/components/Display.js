173d21bde244e79d3e9a8340a30133ebd4e6a598
RaimundEge <ege@niu.edu>
2021-11-06 16:17:57 -0700

    final cleanup

--- Vue/components/Display.js
+++ Vue/components/Display.js
@@ -9,44 +9,40 @@ app.component('display', {
                     <option value="day">last 24 hours</option>
                     <option value="week">last 7 days</option>
                     <option value="month">last 30 days</option>
-                </select>            
-            <line-chart :data="data"></line-chart>
+                </select>         
+            <line-chart :data="data" class="chart" empty="loading data ..."></line-chart>
          </div>`,
     data() {
         return {
             temps: null,
-            range: 'all',
+            range: 'hour',
             oldRange: 'all'
         }
     },
     methods: {
         async getTemps() {
+            NProgress.start()
             var resp = await axios.get('http://192.168.1.23:5000/' + `?range=${this.range}`)
-            // console.log(resp.data);
-            // this.temps = {}
-            // for (var item of resp.data) {
-            //     // console.log(item)
-            //     this.temps[this.format(item.when["$date"])] = item.temp
-            // }  
             this.temps = resp.data         
-            console.log(this.temps)
+            // console.log(this.temps)
+            NProgress.done()
         },
         format(when) {
             return new Date(when).toString().slice(0,22)
         }
     },
     computed: {
-        data() {
+        data() {            
             var data = {}
             if (this.temps == null || this.range !== this.oldRange) {
                 this.getTemps()
                 this.oldRange = this.range
             } else {
                 for (var item of this.temps) {
-                    console.log(item)
+                    // console.log(item)
                     data[this.format(item.when["$date"])] = item.temp
                 } 
-            }  
+            }             
             return data
         }
     }
